b2d08abd15b41e880781548bbd19c991e65cd0ea
timwis <tim@timwis.com>
2016-02-14 12:15:00 -0500

    Use last location in map. Closes #1

--- src/views/map.js
+++ src/views/map.js
@@ -1,19 +1,28 @@
 import View from 'ampersand-view'
 import L from 'leaflet'
 
+import detectStorage from '../util/detect-storage'
 import Template from '../templates/map.html'
 
-const defaultLocation = [37.09024, -95.712891] // Center of USA
+const storageAvailable = detectStorage('localStorage')
+const storedLocation = getStoredLocation()
+const initialLocation = storedLocation || [37.09024, -95.712891] // Center of USA
+const initialZoom = storedLocation ? 16 : 4
+
+function getStoredLocation () {
+  const locationString = storageAvailable && window.localStorage.getItem('lastLocation')
+  return locationString && JSON.parse(locationString)
+}
 
 export default View.extend({
   template: Template,
   render: function () {
     this.renderWithTemplate(this)
-    this.initMap(this.query('#map'))
+    setTimeout(() => this.initMap(this.query('#map')), 100) // TODO: This delay shouldn't be necessary
     return this
   },
   initMap: function (container) {
-    const map = L.map(container).setView(defaultLocation, 4)
+    const map = L.map(container).setView(initialLocation, initialZoom)
     L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
       attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
       maxZoom: 16
@@ -21,12 +30,16 @@ export default View.extend({
 
     map.locate({setView: true, maxZoom: 16})
 
-    map.on('locationfound', this.addLocationRadius(map), this)
+    map.on('locationfound', (e) => {
+      this.createLocationIndicator(e).addTo(map)
+      this.saveLocation(e.latlng)
+    })
   },
-  addLocationRadius: function (map) {
-    return (e) => {
-      const radius = e.accuracy / 2
-      L.circle(e.latlng, radius).addTo(map)
-    }
+  createLocationIndicator: function (e) {
+    const radius = e.accuracy / 2
+    return L.circle(e.latlng, radius)
+  },
+  saveLocation: function (latlng) {
+    if (storageAvailable) window.localStorage.setItem('lastLocation', JSON.stringify(latlng))
   }
 })
